5f80813819b5a82cd1d974e0a07ff44e47e67415
JCGViljoen <jcgviljone51@gmail.com>
2023-05-31 11:24:03 +0200

    question 3

--- script/code.js
+++ script/code.js
@@ -1,7 +1,12 @@
+// declare variable and call by id in body of table
+const displayElement = document.querySelector('#table')
+console.log(displayElement)
+
+// create objects
 let person1 = {
     firstName: 'Seth',
     lastName: 'Kallis',
-    age: '22'
+    age: '102'
 }
 
 let person2 = {
@@ -40,11 +45,62 @@ let person8 = {
     age: '30'
 }
 
-displayElement.innerHTML = `
+
+// display in then html body
+ displayElement.innerHTML = `
 <tr>
-<td>${person1.name}</td>
-<td></td>
-<td></td>
+<td>${person1.firstName}</td>
+<td>${person1.lastName}</td>
+<td>${person1.age}</td>
+
+</tr>
+
+<tr>
+<td>${person2.firstName}</td>
+<td>${person2.lastName}</td>
+<td>${person2.age}</td>
+
+</tr>
+
+<tr>
+<td>${person3.firstName}</td>
+<td>${person3.lastName}</td>
+<td>${person3.age}</td>
+
+</tr>
+
+<tr>
+<td>${person4.firstName}</td>
+<td>${person4.lastName}</td>
+<td>${person4.age}</td>
+
+</tr>
+
+<tr>
+<td>${person5.firstName}</td>
+<td>${person5.lastName}</td>
+<td>${person5.age}</td>
+
+</tr>
+
+<tr>
+<td>${person6.firstName}</td>
+<td>${person6.lastName}</td>
+<td>${person6.age}</td>
+
+</tr>
+
+<tr>
+<td>${person7.firstName}</td>
+<td>${person7.lastName}</td>
+<td>${person7.age}</td>
+
+</tr>
+
+<tr>
+<td>${person8.firstName}</td>
+<td>${person8.lastName}</td>
+<td>${person8.age}</td>
 
 </tr>
 `
