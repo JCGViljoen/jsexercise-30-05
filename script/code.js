2e6d5087bc8311ff96339df5d572ecd35e4c6d4d
JCGViljoen <jcgviljone51@gmail.com>
2023-05-31 10:31:07 +0200

    question 1

---
+++ script/code.js
@@ -0,0 +1,44 @@
+let person1 = {
+    firstName: 'Seth',
+    lastName: 'Kallis',
+    age: '22'
+}
+
+let person2 = {
+    firstName: 'Amy',
+    lastName: 'Pieterse',
+    age: '23'
+}
+let person3 = {
+    firstName: 'Mish-Al',
+    lastName: 'Taliep',
+    age: '21'
+}
+let person4 = {
+    firstName: 'Alonso',
+    lastName: 'Cupido',
+    age: '12'
+}
+let person5 = {
+    firstName: 'Jason',
+    lastName: 'Barron',
+    age: '35'
+}
+let person6 = {
+    firstName: 'Siyamthada',
+    lastName: 'Mazokwana',
+    age: '62'
+}
+let person7 = {
+    firstName: 'Keeno',
+    lastName: 'Fourie',
+    age: '20'
+}
+let person8 = {
+    firstName: 'Logan',
+    lastName: 'Paul',
+    age: '30'
+}
+
+
+  
